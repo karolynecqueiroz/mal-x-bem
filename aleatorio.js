55202a41a253154ca48af177d0508b1dff845da4
Karolyne Queiroz <117736277+karolynecqueiroz@users.noreply.github.com>
2024-08-29 22:34:23 -0300

    Add files via upload

---
+++ aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Teu time é..."];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
---
+++ perguntas.js
@@ -0,0 +1,116 @@
+export const perguntas = [
+    {
+        enunciado: "1. Depois de muito esforço, muitos anos batalhando você consegue alcançar seu objetivo de se tornar jogador de futebol profissional. Você recebeu duas propostas ineditas ?",
+        alternativas: [
+            {
+                texto: "A) O Coringão melhor de todos os tempos",
+                afirmacao: [
+                    " O Tradicionalista. Você é alguém que valoriza o futebol brasileiro, fazendo a melhor escolha possível. Esse é o timão, todo mundo conhece, campeão mundial os Gaviões da Fiel!"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Palmeiras ",
+                afirmacao: [
+                    "Já começou errado, pelo amor de Deus, sem comentarios. Vamos melhorar a seguir."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Você escolheu seu time, a sua primeira contratação para um time profissional. Haverá um jogo, seu time contra o Boca Juniors... Aos 43 minutos do segundo tempo você recebe a bola, é tudo ou nada..",
+        alternativas: [
+            {
+                texto: " Você olha o jogo, do seu lado tem Romero, e na sua frente tem o gol, você CHUTA PRO GOL",
+                afirmacao: [
+                    "GOOOOOOLLLL 'a nova contratação do corinthians é imparavel mesmo hein Casa Grande' com muito nervosismo você faz o gol e é amado pela torcida",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Se você escolheu o palmeiras (clique aqui), você esta no banco de reservas.  ",
+                afirmacao: [
+                    " Que pena né? Vocês tomaram um sacode do boca em plena la bomboneira"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Quando você voltava do jogo recebe uma mensagem:",
+        alternativas: [
+            {
+                texto: "Se você optou pelo corinthians",
+                afirmacao: [
+                    " 'Augusto Melo envou uma mensagem' - Parabéns pelo seu primeiro jogo, decidiu o placar, você é um garoto de ouro, tem muito para viver pela frente menino! Na semana vou te dar uma bonificação, a torcida ja te ama."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Se você optou pelo palmeiras",
+                afirmacao: [
+                    " KKKKKKKKKKKKKKKK PERDERAM, MUITO RUIM. Vacilou demais em ter escolhido esse."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: " Chegou no meio da semana o diretor do seu time te chama para conversar...",
+        alternativas: [
+            {
+                texto: " Augusto Melo (corinthians)",
+                afirmacao: [
+                    "Olha... como prometido vou te premiar como uma bonificação de 350 mil reais pelo seu ótimo protagonismo no seu primeiro pelo jogo, levou nossa torcida a loucura, não via eles assim à um bom tempo, parabéns meu garoto!."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Leila Pereira (palmeiras)",
+                afirmacao: [
+                    " Não trago boas noticias para você, no nosso ultimo jogo não fomos nada bem, o clube anda um alvoroço, por isso vamos ter que reduzir os custos, e lamento dizer... seu contrato está terminado."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Depois de algum tempo você está de férias...",
+        alternativas: [
+            {
+                texto: "Você é jogador do corinthians",
+                afirmacao: [
+                    " 'PARABÉNS VOCÊ FOI PREMIADO COM UMA VIAGEM COM TUDO PAGO PARA OS ESTADOS UNIDOS COM 3 ACOMPANHANTES, APROVEITE!' Diretoria do Timão."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Você é jogador do palmeiras",
+                afirmacao: [
+                    "Suas férias são no sofá da sua casa, aproveite para passar com sua familia, infelizmente seu clube não te premiou com nada após sua saída de lá."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "No fim de tudo, você gostou da sua carreira?",
+        alternativas: [
+            {
+                texto: " SIM, A ELHOR DE TODAS!",
+                afirmacao: [
+                    "Acho que você escolheu o melhor time... o coringão."
+                ],
+            },
+            {
+                texto: "Não, nem tanto.",
+                afirmacao: [
+                    "Palmeiras né... lamento."
+                ],
+            },
+        ]
+    },
+];
+
